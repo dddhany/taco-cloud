2e58c7ad5c5efa66447e469a99e461f88f1735cf
Dhany <dddhany@gmail.com>
2020-04-19 17:26:34 +0800

    user login and registration

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -9,14 +9,14 @@ import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
-	List<Order> findByDeliveryZip(String deliveryZip);
-
-	List<Order> readOrdersByDeliveryZipAndPlaceAtBetween(String deliveryZip, Date startDate, Date endDate);
-
-	List<Order> findByDeliveryToAndDeliveryCityAllIgnoresCase(String deliveryTo, String deliveryCity);
-
-	List<Order> findByDeliveryCityOrderByDeliveryTo(String city);
-	
-	@Query("Order o where o.deliveryCity='Seattle'")
-	List<Order> readOrdersDeliveredInSeattle();
+//	List<Order> findByDeliveryZip(String deliveryZip);
+//
+//	List<Order> readOrdersByDeliveryZipAndPlaceAtBetween(String deliveryZip, Date startDate, Date endDate);
+//
+//	List<Order> findByDeliveryToAndDeliveryCityAllIgnoresCase(String deliveryTo, String deliveryCity);
+//
+//	List<Order> findByDeliveryCityOrderByDeliveryTo(String city);
+//	
+//	@Query("Order o where o.deliveryCity='Seattle'")
+//	List<Order> readOrdersDeliveredInSeattle();
 }
